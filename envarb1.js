1497e991ec773f296a487cd173cad54afca943da
Emelie Segell <segell.emelie@gmail.com>
2018-02-13 12:58:23 +0100

    assignment 1.2

--- envarb1.js
+++ envarb1.js
@@ -1,24 +1,10 @@
-//var http = require ("http");
-//
-//http.createServer (function (request, response ){
-//    response.writeHead(200, {"Content-Type": "text/plain"});
-//    response.write("Hello World");
-//    response.end();
-//}).listen(8888);
-
-
-var http = require ('http');
-var envarb = showObject(process.env);
+var http = require('http');
 
 http.createServer(function(request, response){
-response.writeHead(200, {"Content-Type": "text/plain"});
-response.end(envarb);
+    var envarb = showObject(process.env);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end(envarb);
 }).listen(8888);
-
-function getEnvarbs(){
-     console.log(process.env);
-     return (process.env);
- }; 
 
  function showObject(obj) {
     var result = "";
